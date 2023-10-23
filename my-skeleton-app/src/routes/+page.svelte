c3bbdddb1406e874c82e321d0bc1807544665bb9
Anas099X <Anasshohdy400@gmail.com>
2023-10-23 20:37:22 +0300

    first push

--- my-skeleton-app/src/routes/+page.svelte
+++ my-skeleton-app/src/routes/+page.svelte
@@ -1,22 +1,46 @@
-<head>
+
 <link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" />
 <script defer src="https://pyscript.net/latest/pyscript.js">
+	import {  AppBar, Accordion, AccordionItem, AppShell} from '@skeletonlabs/skeleton';
 </script>
-</head>
 
-<py-script>pyscript.write('test1',"Masjed Library")</py-script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<!-- svelte-ignore a11y-missing-content -->
-		<h1 class="h1" id="test1"></h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
+
+
+
+
+
+
+		
+   <AppBar>
+	<svelte:fragment slot="lead" ><div>🕌</div></svelte:fragment>
+	<h2 class="h2" style="">
+		<span class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone">Masjid</span>
+		</h2>
+	<svelte:fragment slot="trail"><button type="button" class="btn variant-ghost-primary">Discord</button></svelte:fragment>
+   </AppBar>
+
+
+	<slot />
+	<!-- ---- / ---- -->
+	<div class="card p-4" style=" position:relative; left:25%; top:100px; width:50%;">
+		
+<Accordion>
+	<AccordionItem open>
+		<svelte:fragment slot="lead">📘</svelte:fragment>
+		<svelte:fragment slot="summary">Quran</svelte:fragment>
+		<svelte:fragment slot="content">This section would have Quran and Tafsier</svelte:fragment>
+	</AccordionItem>
+	<AccordionItem>
+		<svelte:fragment slot="lead">📕</svelte:fragment>
+		<svelte:fragment slot="summary">Hadith</svelte:fragment>
+		<svelte:fragment slot="content">This section would have Hadith Books</svelte:fragment>
+	</AccordionItem>
+	<!-- ... -->
+</Accordion>
+
+		
+		
 	</div>
-</div>
+
+
